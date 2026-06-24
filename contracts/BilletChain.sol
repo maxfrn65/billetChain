54539baf5565b96bb60d2543f4cba27a6d10434b
Maxime Fourna <m.fourna@icloud.com>
2026-06-24 15:20:58 +0200

    feat: billetChain.sol

--- contracts/BilletChain.sol
+++ contracts/BilletChain.sol
@@ -28,6 +28,39 @@ contract BilletChain is ERC721 {
 
     mapping(uint256 => uint256) public initialPriceInEur;
 
+    error InvalidPrice();
+    error StalePrice();
+
+    function getLatestPrice() public view returns (int256 price, uint8 decimals) {
+        (, int256 answer, , uint256 updatedAt, ) = priceFeed.latestRoundData();
+
+        if (answer <= 0) revert InvalidPrice();
+        if (block.timestamp - updatedAt > MAX_DELAY) revert StalePrice();
+
+        return (answer, priceFeed.decimals());
+    }
+
+    mapping(address => uint256) public pendingWithdrawals;
+
+    function eurToEth(uint256 eurAmount) public view returns (uint256) {
+        (int256 price, uint8 decimals) = getLatestPrice();
+        return (eurAmount * 1e18 * (10 ** decimals)) / uint256(price);
+    }
+
+    function buyTicket() external payable {
+        require(nextTokenId < maxTickets, "Evenement complet");
+
+        uint256 ethAmount = eurToEth(ticketPriceinEur);
+
+        require(msg.value == ethAmount, "Paiement incorrect");
+
+        initialPriceInEur[nextTokenId] = ticketPriceinEur;
+        pendingWithdrawals[organizer] += msg.value;
+
+        _safeMint(msg.sender, nextTokenId);
+        nextTokenId++;
+    }
+
     constructor(address _priceFeed, uint256 _maxTickets, uint256 _ticketPriceinEur) ERC721("BilletChain", "TICKET") {
         organizer = msg.sender;
         priceFeed = IPriceFeed(_priceFeed);
